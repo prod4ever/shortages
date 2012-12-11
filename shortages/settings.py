6b73c1d87fccac5c452211c7d690e116bd13fb94
= <lincoln@prod4ever.com>
2012-12-10 19:16:11 -0500

    reqs

--- shortages/settings.py
+++ shortages/settings.py
@@ -3,15 +3,5 @@ from settings_common import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'shortages',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
-
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
