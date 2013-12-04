0b8a33a0437a39e2ff31483a72e1c77c48952d61
Jeremy Lach <lach.jeremy@gmail.com>
2013-12-04 17:31:34 -0500

    updated

--- shortages/scraping/management/commands/scrape.py
+++ shortages/scraping/management/commands/scrape.py
@@ -36,7 +36,7 @@ class Command(BaseCommand):
         page = self.fetch(url)
         self.soup = BeautifulSoup(page, 'html5lib')
         print self.soup
-        drug_tables = self.soup.find_all('.middle-column table')
+        drug_tables = self.soup.find_all("a")#('.middle-column table')
         #drug_tables = self.soup.select('table[summary="drug shortage details"]');
         print drug_tables
 
